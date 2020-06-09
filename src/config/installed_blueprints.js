1ef76ec5dbc32954dffadf851720d820670ceefb
Crowdbotics <team@crowdbotics.com>
2020-06-09 08:19:13 +0000

    add package blueprint-react-native-blank-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'BlankScreen25284', human_name: 'Blank Screen 2', access_route: 'BlankScreen25284'},
 { name: 'BlankScreen35283', human_name: 'Blank Screen 3', access_route: 'BlankScreen35283'},
 { name: 'EmailAuth', human_name: 'EmailAuth', access_route: 'EmailAuth', icon: 'envelope-o'},
 
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import BlankScreen25284Navigator from '../features/BlankScreen25284/navigator';
 import BlankScreen35283Navigator from '../features/BlankScreen35283/navigator';
 import EmailAuthNavigator from '../features/EmailAuth/navigator';
 
@@ -17,6 +18,7 @@ const AppNavigator = {
       screen: SplashScreen
     },
     //@BlueprintNavigationInsertion
+BlankScreen25284: { screen: BlankScreen25284Navigator },
 BlankScreen35283: { screen: BlankScreen35283Navigator },
 EmailAuth: { screen: EmailAuthNavigator },
 

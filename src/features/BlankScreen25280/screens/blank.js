d4bdead925972f72eef4acbe97d2205e51ac94cd
Crowdbotics <team@crowdbotics.com>
2020-06-09 07:16:09 +0000

    Update blank.js JSX

--- src/features/BlankScreen25280/screens/blank.js
+++ src/features/BlankScreen25280/screens/blank.js
@@ -1,19 +1,74 @@
-import React from "react";
-import { View } from "react-native";
-import { withStyles, Text, Button, CheckBox, Radio, Toggle, Icon, Input } from 'react-native-ui-kitten';
+import React from "react"
+import { View } from "react-native"
+import {
+  withStyles,
+  Text,
+  Button,
+  CheckBox,
+  Radio,
+  Toggle,
+  Icon,
+  Input
+} from "react-native-ui-kitten"
 
 export class _Blank extends React.Component {
   static navigationOptions = {
     title: "Blank".toUpperCase()
-  };
+  }
 
-  state = { }
+  state = { Radio_5: true, CheckBox_6: true }
 
-  render = () => <View style={this.props.themedStyle.container}></View>;
+  render = () => (
+    <View
+      style={{
+        overflow: "visible",
+        flexDirection: "column",
+        flexWrap: "wrap",
+        justifyContent: "flex-start",
+        alignItems: "stretch",
+        alignContent: "stretch",
+        borderStyle: "solid",
+        backgroundSize: "auto"
+      }}
+    >
+      <Button
+        style={{
+          overflow: "visible",
+          borderStyle: "solid",
+          backgroundColor: "#3366FF"
+        }}
+        onPress={() => alert("Pressed!")}
+      >
+        Button
+      </Button>
+      <Text style={{ overflow: "visible", borderStyle: "solid" }}>Text</Text>
+      <Input
+        date
+        placeholder="Text input placeholder"
+        editable={true}
+        value=""
+        style={{ overflow: "visible", borderStyle: "solid" }}
+      />
+      <Radio
+        text="Radio button"
+        disabled={false}
+        style={{ overflow: "visible", borderStyle: "solid" }}
+        checked={this.state.Radio_5}
+        onChange={nextChecked => this.setState({ Radio_5: nextChecked })}
+      />
+      <CheckBox
+        text="Checkbox"
+        disabled={false}
+        style={{ overflow: "visible", borderStyle: "solid" }}
+        checked={this.state.CheckBox_6}
+        onChange={nextChecked => this.setState({ CheckBox_6: nextChecked })}
+      />
+    </View>
+  )
 }
 
 export default Blank = withStyles(_Blank, theme => ({
   container: {
     backgroundColor: theme["color-basic-100"]
   }
-}));
+}))
